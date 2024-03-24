12152523da3ebbd852b81dddc3946adb2c4f5e8d
YashwanthKumar02 <yashwanthnani2@gmail.com>
2024-03-25 00:50:02 +0530

    Image Transformations

--- transformations.py
+++ transformations.py
@@ -19,6 +19,30 @@ def translate(img, x, y):
 translated = translate(img, 100, 100)
 cv.imshow('Translated', translated)
 
+#Rotation
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+cv.imshow('Rotated', rotated)
+
+#Resizing
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+#Flipping
+flip = cv.flip(img, 0)
+cv.imshow('Flipped', flip)
+
+#Cropping
+cropped = img[200:400, 300:400]
+cv.imshow('Cropped', cropped)
+
 
 
 cv.waitKey(0)
